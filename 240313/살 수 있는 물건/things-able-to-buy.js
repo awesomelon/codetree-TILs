96d69ba886f6981a9e1cb09dcc0c694273b6c1aa
JAEHO BANG <awesomelon@naver.com>
2024-03-13 13:27:59 +0900

    Title: 살 수 있는 물건, Time: 172ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240313/살 수 있는 물건/things-able-to-buy.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+const n = Number(input);
+
+
+if(n >= 3000) {
+    console.log('book');
+} else if( n >= 1000) {
+    console.log('mask');
+} else {
+    console.log('n');
+}
