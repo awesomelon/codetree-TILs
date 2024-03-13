b8686de4441e4ff85e54609bac56830df20f1471
JAEHO BANG <awesomelon@naver.com>
2024-03-13 13:50:28 +0900

    Title: 중앙값 구하기, Time: 164ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240313/중앙값 구하기/find-the-median.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const [a, b, c] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(v => +v);
+
+if(a > b && a < c) {
+    console.log(a);
+}
+
+if(b > a && b < c) {
+    console.log(b);
+}
+
+if(c > a && c < b) {
+    console.log(c);
+}
