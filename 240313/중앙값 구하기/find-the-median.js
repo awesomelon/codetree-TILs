da1da3e4a3901849fe7bb21781c52fe3eeafb54b
JAEHO BANG <awesomelon@naver.com>
2024-03-13 13:51:50 +0900

    Title: 중앙값 구하기, Time: 231ms, Memory: 14MB, Status: Passed - CodeTree

--- 240313/중앙값 구하기/find-the-median.js
+++ 240313/중앙값 구하기/find-the-median.js
@@ -5,10 +5,22 @@ if(a > b && a < c) {
     console.log(a);
 }
 
+if(a > c && a < b) {
+    console.log(a);
+}
+
 if(b > a && b < c) {
+    console.log(b);
+}
+
+if(b > c && b < a) {
     console.log(b);
 }
 
 if(c > a && c < b) {
     console.log(c);
+}
+
+if(c > b && c < a) {
+    console.log(c)
 }
