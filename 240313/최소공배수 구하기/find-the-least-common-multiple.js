1265ac07bdb3039b3e6d75f2ed70106721b8a40e
JAEHO BANG <awesomelon@naver.com>
2024-03-13 19:24:02 +0900

    Title: 최소공배수 구하기, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240313/최소공배수 구하기/find-the-least-common-multiple.js
+++ 240313/최소공배수 구하기/find-the-least-common-multiple.js
@@ -3,8 +3,8 @@ const [n, m] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Nu
 
 let result = 0;
 
-for(let i = n; i < m*n; i++) {
-    if(i % n === 0 && i % m === 0) {
+for(let i = m; i < m*n; i++) {
+    if(i % n === 0 && i % m === 0) {        
         result = i;
         break;
     }
