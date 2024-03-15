c147caabc1377615fa1022f3da7fe93a2af3cbba
JAEHO BANG <awesomelon@naver.com>
2024-03-15 10:22:16 +0900

    Title: 최대공약수 구하기, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240315/최대공약수 구하기/find-the-greatest-common-divisor.js
+++ 240315/최대공약수 구하기/find-the-greatest-common-divisor.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const [n, m] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
 
 function gcd() {
-    let result = 0;
+    let result = 1;
     for(let i = 1; i < Math.min(n,m); i++) {
         if(n % i === 0 && m % i === 0) {
             result = i;
