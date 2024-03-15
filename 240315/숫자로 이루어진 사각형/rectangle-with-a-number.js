e800893c01e819b484f22b811e7992fccee950b5
JAEHO BANG <awesomelon@naver.com>
2024-03-15 10:13:44 +0900

    Title: 숫자로 이루어진 사각형, Time: 345ms, Memory: 22MB, Status: Passed - CodeTree

--- 240315/숫자로 이루어진 사각형/rectangle-with-a-number.js
+++ 240315/숫자로 이루어진 사각형/rectangle-with-a-number.js
@@ -3,15 +3,20 @@ const input = fs.readFileSync('/dev/stdin').toString().trim();
 
 const N = Number(input);
 
-let number = 1;
+function printSquare(n) {
+    let number = 1;
 
-for(let i = 1; i <= N * N; i++) {
-    if(number > 9) {
-        number = 1;
+    for(let i = 1; i<= N*N; i++) {
+        if(number > 9) {
+            number = 1;
+        }
+
+        process.stdout.write(`${number} `);
+        if(i % N === 0) {
+            console.log();
+        }
+        number++;
     }
-    process.stdout.write(`${number} `);
-    if(i % N === 0) {
-        console.log();
-    }
-    number++;
 }
+
+printSquare(N);
