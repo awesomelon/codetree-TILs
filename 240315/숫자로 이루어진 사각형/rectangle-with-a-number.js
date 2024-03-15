f08e703600854786ec94bacd55d5356b8d6f498c
JAEHO BANG <awesomelon@naver.com>
2024-03-15 10:08:37 +0900

    Title: 숫자로 이루어진 사각형, Time: 353ms, Memory: 22MB, Status: Wrong Answer - CodeTree

--- 240315/숫자로 이루어진 사각형/rectangle-with-a-number.js
+++ 240315/숫자로 이루어진 사각형/rectangle-with-a-number.js
@@ -6,7 +6,7 @@ const N = Number(input);
 for(let i = 1; i <= N * N; i++) {    
     let number = i > 9 ? i - 9: i;  
     process.stdout.write(`${number} `);
-    if(i % 4 === 0) {
+    if(i % N === 0) {
         console.log();
     }  
 }
