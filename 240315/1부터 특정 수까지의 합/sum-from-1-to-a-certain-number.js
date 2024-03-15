8d4761495415ecd82ba8ebbb2d37e399fa3a9d97
JAEHO BANG <awesomelon@naver.com>
2024-03-15 10:33:08 +0900

    Title: 1부터 특정 수까지의 합, Time: 199ms, Memory: 14MB, Status: Passed - CodeTree

--- 240315/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.js
+++ 240315/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync('/dev/stdin').toString().trim();
 const N = Number(input);
 
 function sum(n) {
-    return ((1 + n) * n / 2) / 10;
+    return parseInt(((1 + n) * n / 2) / 10);
 }
 
 console.log(sum(N));
