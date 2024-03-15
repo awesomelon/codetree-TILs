ea0daf48c65516cee776c4c1ec3a1d72fa6d239f
JAEHO BANG <awesomelon@naver.com>
2024-03-15 10:02:58 +0900

    Title: 반복 출력하기, Time: 201ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240315/반복 출력하기/repeated-output.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+const N = Number(input);
+
+for(let i = 0; i < N; i++) {
+    console.log('12345^&*()_')
+}
