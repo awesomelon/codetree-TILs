26cf99114c15ecd64fddbe62cd394dfbaa5255b1
JAEHO BANG <awesomelon@naver.com>
2024-03-15 16:01:56 +0900

    Title: 신기한 타일 뒤집기, Time: 204ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240315/신기한 타일 뒤집기/strange-flipping-tiles.js
+++ 240315/신기한 타일 뒤집기/strange-flipping-tiles.js
@@ -3,27 +3,28 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const n = Number(input[0]);
 
-let x = 0;
-let white_tile = 0;
-let black_tile = 0;
-
+let tiles = [];
+let cursor = 50;
 for(let i = 1; i < input.length; i++) {
     const [m, location] = input[i].split(' ');
     const move = Number(m);
-    if(location === 'R') {
-        x += move;
-        black_tile += move;
-
-        white_tile = white_tile - move > 0 ? white_tile - move: 0;
+    if(location === 'R') {        
+       for(let i = cursor; i < cursor + move; i++) {
+            tiles[i] = 'B';
+        }
+       cursor += move - 1;
     }
 
     if(location === 'L') {
-        x -= move;
-        white_tile = move;
-
-        black_tile = black_tile - move > 0 ? black_tile - move: 0;
+        for(let i = cursor; i > cursor - move; i--) {
+            tiles[i] = 'W';
+        }
+       cursor -= move;
     }
 }
 
+const W = tiles.filter(v => v === 'W').length;
+const B = tiles.filter(v => v === 'B').length;
 
-console.log(`${white_tile} ${black_tile}`);
+
+console.log(`${W} ${B}`);
