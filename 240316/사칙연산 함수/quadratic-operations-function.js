e3bc89c9a560a3fab200eff8dae46dd3f18d9dd7
JAEHO BANG <awesomelon@naver.com>
2024-03-16 22:08:13 +0900

    Title: 사칙연산 함수, Time: 164ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240316/사칙연산 함수/quadratic-operations-function.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const [a, b, c] = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+
+function operation(a, b, c) {
+    const n1 = parseInt(a);
+    const n2 = parseInt(c);
+
+    const oper = `${n1} ${b} ${n2} =`;
+
+    if(b === '*') {
+        return `${oper} ${n1 * n2}`;
+    }
+
+    if(b === '+') {
+        return `${oper} ${n1 + n2}`;
+    }
+
+    if(b === '-') {
+        return `${oper} ${n1 - n2}`;
+    }
+
+    if(b === '/') {
+        return `${oper} ${n1 / n2}`;
+    }
+}
+
+console.log(operation(a,b,c));
