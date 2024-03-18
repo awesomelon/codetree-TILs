93ff336ea762158edb1547c89179248363c814e2
JAEHO BANG <awesomelon@naver.com>
2024-03-18 15:18:33 +0900

    Title: 1이 되는 순간까지, Time: 169ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240318/1이 되는 순간까지/until-the-moment-I-reach-one.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+const N = Number(input);
+
+function recur(n, s) {
+    if(n === 1) {
+        return s;
+    }
+
+    return recur(n % 2 ? parseInt(n / 2) : parseInt(n / 3), s + 1);
+}
+
+console.log(recur(N, 1));
