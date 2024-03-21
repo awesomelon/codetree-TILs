ce1dfc5de3ecbb18d0f4a2f3b9decca76affda40
JAEHO BANG <awesomelon@naver.com>
2024-03-21 17:41:26 +0900

    Title: 두 개의 동일한 수열, Time: 169ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240321/두 개의 동일한 수열/two-equal-series.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const asc = (a, b) => a - b;
+
+const A = input[1].split(' ').map(Number).sort(asc).join('');
+const B = input[2].split(' ').map(Number).sort(asc).join('');
+
+
+console.log(A === B ? 'Yes': "No");
