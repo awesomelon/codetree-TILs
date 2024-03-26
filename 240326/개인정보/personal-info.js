e798d9fbae49cee07f3401ce1f003aad7184400f
JAEHO BANG <awesomelon@naver.com>
2024-03-26 14:04:09 +0900

    Title: 개인정보, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240326/개인정보/personal-info.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const persons = fs.readFileSync(0).toString().trim().split('\n');
+
+class Person {
+    name;
+    height;
+    weight;
+
+    constructor(name, height, weight) {
+        this.name = name;
+        this.height = parseFloat(height);
+        this.weight = parseFloat(weight);
+    }
+}
+
+const personsByName = persons.map(person => new Person(...person.split(' '))).sort((a,b) => {
+    if(a.name > b.name) {
+        return 1;
+    }
+    if(a.name < b.name) {
+        return -1;
+    }
+    return 0;
+});
+
+const personsByHeight = persons.map(person => new Person(...person.split(' '))).sort((a,b) => {
+    return b.height - a.height;
+});
+
+console.log('name');
+personsByName.forEach(person => {
+    console.log(`${person.name} ${person.height} ${person.weight}`);
+});
+
+console.log()
+
+console.log('height');
+personsByHeight.forEach(person => {
+    console.log(`${person.name} ${person.height} ${person.weight}`);
+});
