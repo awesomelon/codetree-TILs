08ba6446dab989c615d6b7e1a6782345b1476152
JAEHO BANG <awesomelon@naver.com>
2024-03-26 17:11:09 +0900

    Title: Time to Time, Time: 204ms, Memory: 14MB, Status: Passed - CodeTree

--- 240326/Time to Time/time-to-time.js
+++ 240326/Time to Time/time-to-time.js
@@ -1,20 +1,4 @@
 const fs = require("fs");
 const [a,b,c,d] = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 
-let prev_a = a, prev_b = b, after_c = c, after_d = d;
-let minutes = 0;
-while(true) {    
-    if(prev_a === after_c && prev_b === after_d) {
-        console.log(minutes)
-        return;
-    }
-
-    prev_b++;
-    minutes++;
-
-    if(prev_b > 59) {
-        prev_b = 0;
-        prev_a++;
-    }
-
-}
+console.log((c * 60 + d )- (a * 60 + b))
