fba11054ba7b0ea768c8e8df95aeb8e2da184671
JAEHO BANG <awesomelon@naver.com>
2024-03-27 15:55:21 +0900

    Title: 요일 맞추기, Time: 212ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240327/요일 맞추기/guess-day-of-week.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const [m1, d1, m2, d2] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+const dates = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', "Sat"];
+const startAt = new Date(`2011-${m1}-${d1}`).getTime();
+const targetAt = new Date(`2011-${m2}-${d2}`).getTime();
+
+
+let diff = (targetAt - startAt) / 1000 / 60 / 60 / 24;
+let cursor = 1;
+
+while(diff) {
+    if(diff < 0) {
+        diff++;
+        cursor--;
+    }
+
+    if(diff > 0) {
+        diff--;
+        cursor++;
+    }
+
+    if(cursor === 6) {
+        cursor = 0;
+    }
+}
+
+console.log(dates[cursor]);
