06b6dfcc21fb33e3d1b4633a2d88935ab88c89c8
JAEHO BANG <awesomelon@naver.com>
2024-04-02 15:00:57 +0900

    Title: 연속되는 수 2, Time: 188ms, Memory: 15MB, Status: Passed - CodeTree

--- 240402/연속되는 수 2/continuous-number2.js
+++ 240402/연속되는 수 2/continuous-number2.js
@@ -3,7 +3,7 @@ const [n, ...numbers] = fs.readFileSync(0).toString().trim().split('\n').map(Num
 
 let maxCnt = 1;
 let tempCnt = 1;
-for(let i = 2; i <= n; i++) {
+for(let i = 0; i < n; i++) {
     const current = numbers[i];
     const prev = numbers[i - 1];
     if(prev === current) {
@@ -11,7 +11,7 @@ for(let i = 2; i <= n; i++) {
     }
 
     if(prev !== current) {
-        tempCnt = 0;
+        tempCnt = 1;
     }
 
     if(maxCnt < tempCnt) {
