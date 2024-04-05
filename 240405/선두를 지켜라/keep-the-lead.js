a2285a4abfe7bea53ad212a6cd707bfa90908a0a
JAEHO BANG <awesomelon@naver.com>
2024-04-05 14:54:02 +0900

    Title: 선두를 지켜라, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240405/선두를 지켜라/keep-the-lead.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const log = console.log;
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+let line = 1;
+
+const A = [0];
+const B = [0];
+let cursor = 0;
+for(let i = 0; i < n; i++) {    
+    const [v, t] = input[line++].split(' ').map(Number);    
+    for(let j = 1; j <= t; j++) {
+        cursor+=v;
+        A.push(cursor);
+    }
+}
+
+cursor = 0;
+for(let i = 0; i < m; i++) {
+    const [v, t] = input[line++].split(' ').map(Number);        
+    for(let j = 1; j <= t; j++) {
+        cursor+=v;
+        B.push(cursor);
+    }
+}
+
+let first = null;
+let result = 0;
+
+for(let i = 0; i < A.length; i++) {
+    const a = A[i];
+    const b = B[i];
+
+    if(!first) {
+        first = a > b ? 'A' : 'B';
+    } else {
+        if(a > b && first === 'B') {
+            first = 'A';
+            result++;
+        } else if(a < b && first === 'A') {
+            first = 'B';
+            result++;
+        }
+    }
+}
+
+log(result)
