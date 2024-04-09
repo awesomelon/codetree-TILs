9187849c0a01d20d2ef5f73e19aea17fd5f21320
JAEHO BANG <awesomelon@naver.com>
2024-04-09 16:11:23 +0900

    Title: 문자에 따른 명령 2, Time: 194ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 240409/문자에 따른 명령 2/text-based-commands2.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const log = console.log;
+const input = fs.readFileSync(0).toString().trim();
+
+let x = 0, y = 0;
+let heads = ['N', 'E', 'S', 'W'];
+let cursor = 0;
+
+for(const a of input) {
+    if(a === 'L') {
+        cursor = cursor === 0 ? 3: cursor--;
+    }
+
+    if(a === 'R') {
+        cursor = cursor === 3 ? 0: cursor++;
+    }
+
+    if(a === 'F') {
+        const head = heads[cursor];
+        if(head === 'N') {
+            y++;
+        }
+
+        if(head === 'E') {
+            x++;
+        }
+
+        if(head === 'S') {
+            y--;
+        }
+
+        if(head === 'W') {
+            x--;
+        }
+    }
+}
+
+log(`${x} ${y}`);
