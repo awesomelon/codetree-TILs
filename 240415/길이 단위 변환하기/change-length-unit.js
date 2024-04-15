ea9c8d8526cff8a9e157a59a496d1e2c31909379
JAEHO BANG <awesomelon@naver.com>
2024-04-15 16:04:33 +0900

    Title: 길이 단위 변환하기, Time: 217ms, Memory: 14MB, Status: Passed - CodeTree

--- 240415/길이 단위 변환하기/change-length-unit.js
+++ 240415/길이 단위 변환하기/change-length-unit.js
@@ -5,5 +5,5 @@ const input = fs.readFileSync(0).toString().trim();
 const ft = 30.48;
 const mi = 160934;
 
-log(`9.2ft = ${(ft * 9.2).toFixed(1)}`);
-log(`1.3mi = ${(mi * 1.3).toFixed(1)}`);
+log(`9.2ft = ${(ft * 9.2).toFixed(1)}cm`);
+log(`1.3mi = ${(mi * 1.3).toFixed(1)}cm`);
