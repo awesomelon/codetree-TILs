ad9d281fb6a05218687db4b1d850acfc069688d5
JAEHO BANG <awesomelon@naver.com>
2024-04-15 16:04:27 +0900

    Title: 길이 단위 변환하기, Time: 216ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240415/길이 단위 변환하기/change-length-unit.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const log = console.log;
+const input = fs.readFileSync(0).toString().trim();
+
+const ft = 30.48;
+const mi = 160934;
+
+log(`9.2ft = ${(ft * 9.2).toFixed(1)}`);
+log(`1.3mi = ${(mi * 1.3).toFixed(1)}`);
