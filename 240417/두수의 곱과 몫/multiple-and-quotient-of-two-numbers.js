2ec0a2ca657fc4f40d988dc7eaf0ac1debaf3319
JAEHO BANG <awesomelon@naver.com>
2024-04-17 13:53:15 +0900

    Title: 두수의 곱과 몫, Time: 189ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240417/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const log = console.log;
+const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+log(`${a} * ${b} = ${a * b}`);
+log(`${a} / ${b} = ${parseInt(a / b)}`)
