5d27331d0d501983f3fb35566c03c2fcca3ba6aa
JAEHO BANG <awesomelon@naver.com>
2024-05-03 09:31:32 +0900

    Title: 범위 밖의 수, Time: 246ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240503/범위 밖의 수/number-out-of-range.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const log = console.log;
+const input = fs.readFileSync(0).toString().trim();
+
+const a = Number(input);
+
+if(a < 10 || a > 20) {
+    log('yes')
+} else {
+    log('no');
+}
