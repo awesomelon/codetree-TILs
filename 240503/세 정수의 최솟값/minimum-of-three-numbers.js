5e1e526abf9b4792f425a3567005f640e4aace54
JAEHO BANG <awesomelon@naver.com>
2024-05-03 09:30:03 +0900

    Title: 세 정수의 최솟값, Time: 260ms, Memory: 14MB, Status: Passed - CodeTree

--- 240503/세 정수의 최솟값/minimum-of-three-numbers.js
+++ 240503/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -4,12 +4,8 @@ const [a, b, c] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
 if(a <= b && a <= c) {
     log(a);
-} 
-
-if(b <= a && b <= c) {
+} else if(b <= a && b <= c) {
     log(b)
-}
-
-if(c <= a && c <= b) {
+} else {
     log(c)
 }
