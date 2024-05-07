7f728da0d3e9662ff2db5c970ba93f127fed46aa
JAEHO BANG <awesomelon@naver.com>
2024-05-07 09:35:38 +0900

    Title: 좀 더 어려운 수학 점수, Time: 167ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240507/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const log = console.log;
+const [A, B] = fs.readFileSync(0).toString().trim().split('\n');
+
+const [math_a, eng_a] = A.split(' ').map(Number);
+const [math_b, eng_b] = B.split(' ').map(Number);
+
+if(math_a > math_b) {
+    log('A');
+    return;
+}
+
+if(math_a < math_b) {
+    log('B');
+    return;
+}
+
+if(eng_a > eng_b) {
+    log('A');
+    return;
+}
+
+if(eng_a < eng_b) {
+    log('B');
+    return;
+}
