53bd9e42a0606f826d60b75dedf6f61464d7002e
JAEHO BANG <awesomelon@naver.com>
2024-05-08 09:35:42 +0900

    Title: 1까지 나누기, Time: 251ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240508/1까지 나누기/divide-by-1.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const log = console.log;
+const input = fs.readFileSync(0).toString().trim();
+
+let n = Number(input);
+
+let count = 0;
+let division = 1;
+
+while(n > 0) {
+    n =  parseInt(n / division);
+    division++;
+    count++;
+}
+log(count);
